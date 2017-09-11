622d00136573689211f339ed34f5b0e9e72bc9ae
Nikhil.Thampi <Nikhil.Sherinethampi@phe.gov.uk>
2017-09-11 10:44:08 +0100

    added pascal triangle

---
+++ Pascalstriangle.scala
@@ -0,0 +1,22 @@
+package src.main
+
+object Pascalstriangle {
+  def main(args: Array[String]) =
+  {
+    println("Pascal's Traingle")
+
+    for(row <- 0 to 10)
+    {
+      for(col <- 0 to row)
+        print(pascal(col,row) + "")
+      println()
+    }
+
+  }
+
+  def pascal(c:Int,r:Int):Int =
+  {
+    if(c==0 || c == r) 1
+    else pascal(c-1,r-1) + pascal(c,r-1)
+  }
+}
